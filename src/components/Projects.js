1bd013a00f201d77955c78190c632ae2d57f6d1e
Kevin Babakhani <kevinbabakhani@yahoo.com>
2025-02-15 13:31:14 -0800

    Replaced resume and changed project order.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,6 +12,12 @@ function Projects() {
                 </a>
             </div>
             <div className="project">
+                <h3><u>LuminosityLEDs — React/NodeJS/ExpressJS/
+                TypeScript/MongoDB/NextJS </u></h3>
+                <p>Software developer for the backend team of luminosityleds. Implemented notification service via code deployment to production cloud service.</p>
+                <p><strong>Github Repo:</strong> <a href="https://github.com/luminosityleds/server" target="_blank" rel="noopener noreferrer">luminosityleds (server) Repo</a></p>
+            </div>
+            <div className="project">
                 <h3><u>SerenityNest — Flutter/Dart/Firebase </u></h3>
                 <p>Creation of a mental health android app called SerenityNest using Dart and Flutter on Android Studio to improve the mental health of all users. Includes user authorization functionality using Firebase.</p>
                 <p><strong>GitHub Repo:</strong> <a href="https://github.com/KevinLemon112/serenity_nest" target="_blank" rel="noopener noreferrer">SerenityNest Repo</a></p>
@@ -22,12 +28,6 @@ function Projects() {
                 <p>Creation of Cal Poly Pomona bronco dining guide website to help students understand food spots around campus (made during CPP’s 2024 BroncoHacks Hackathon).</p>
                 <p><strong>GitHub Repo:</strong> <a href="https://github.com/BroncoHacks-2024-Web-Dev-Bit-Busters/Backend-Frontend" target="_blank" rel="noopener noreferrer">CPP Bronco Dining Guide Repo</a></p>
                 <p><strong>Website:</strong> <a href="https://broncohacks-2024-web-dev-bit-busters.github.io/Backend-Frontend/" target="_blank" rel="noopener noreferrer">CPP Bronco Dining Guide</a></p>
-            </div>
-            <div className="project">
-                <h3><u>LuminosityLEDs — React/NodeJS/ExpressJS/
-                TypeScript/MongoDB/NextJS </u></h3>
-                <p>Software developer for the backend team of luminosityleds. Implemented notification service via code deployment to production cloud service.</p>
-                <p><strong>Github Repo:</strong> <a href="https://github.com/luminosityleds/server" target="_blank" rel="noopener noreferrer">luminosityleds (server) Repo</a></p>
             </div>
         </section>
     );
